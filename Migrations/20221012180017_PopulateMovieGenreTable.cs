c427b16d938ac2538ccf9e29c3b3051de31b565c
mazyn <gilmarfilho75@gmail.com>
2022-10-12 15:04:23 -0300

    feat(movies): :sparkles: populate movie genre table

---
+++ Migrations/20221012180017_PopulateMovieGenreTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly.Migrations
+{
+    public partial class PopulateMovieGenreTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "MovieGenre",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Comedy" });
+
+            migrationBuilder.InsertData(
+                table: "MovieGenre",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Action" });
+
+            migrationBuilder.InsertData(
+                table: "MovieGenre",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Family" });
+
+            migrationBuilder.InsertData(
+                table: "MovieGenre",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 4, "Romance" });
+
+            migrationBuilder.InsertData(
+                table: "MovieGenre",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 5, "Drama" });
+
+            migrationBuilder.InsertData(
+                table: "MovieGenre",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 6, "Horror" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("TRUNCATE TABLE \"MovieGenre\" CASCADE");
+        }
+    }
+}
