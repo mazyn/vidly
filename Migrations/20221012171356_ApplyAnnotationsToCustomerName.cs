a82c680ac850b0c08ddb06d1d277a7eb601a20d3
mazyn <gilmarfilho75@gmail.com>
2022-10-12 14:23:46 -0300

    feat(customers): :sparkles: apply annotations to name property

---
+++ Migrations/20221012171356_ApplyAnnotationsToCustomerName.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly.Migrations
+{
+    public partial class ApplyAnnotationsToCustomerName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Customers",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Customers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+        }
+    }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Vidly.Models
 {
     public record Customer
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        [Required] [StringLength(255)] public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
